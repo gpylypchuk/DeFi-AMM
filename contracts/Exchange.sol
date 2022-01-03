a0884819c201709a4b0c390e0aa19bc53d00a003
Geronimo Pylypchuk <geronimopylypchuk1@gmail.com>
2022-01-03 16:31:12 -0300

    Token to Token Swap!

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -4,15 +4,18 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./Registry.sol";
 
 contract Exchange is ERC20 {
 
     address public tokenAddress;
+    address public registryAddress;
 
     constructor(address _token) ERC20("Exchange LP SKM-ETH", "LP") {
         // Check if the token is a ERC20 (You can check more!).
         require(_token != address(0), "Invalid Token Address");
         tokenAddress = _token;
+        registryAddress = msg.sender;
     }
 
     function addLiquidity(uint256 _tokenAmount) 
@@ -95,12 +98,43 @@ contract Exchange is ERC20 {
         IERC20(tokenAddress).transfer(msg.sender, tokensBought);
     }
 
-    function tokenToEthSwap(uint256 _tokenSold, uint256 _minEth) public {
+    function ethToToken(uint256 _minTokens, address _recipient) private {
         uint256 tokenReserve = getReserve();
-        uint256 ethBought = _getAmount(_tokenSold, tokenReserve, address(this).balance);
-        require(ethBought >= _minEth, "The Ether you will receive are not enough to your minimun exchange amount.");
-        IERC20(tokenAddress).transferFrom(msg.sender, address(this), _tokenSold);
-        payable(msg.sender).transfer(ethBought);
+        uint256 tokensBought = _getAmount(
+            msg.value, 
+            address(this).balance - msg.value, 
+            tokenReserve
+            );
+        require(tokensBought >= _minTokens, "The tokens you will receive are not enough to your minimun exchange amount.");
+        IERC20(tokenAddress).transfer(_recipient, tokensBought);
     }
+
+    function tokenToEthSwap(uint256 _minTokens) public payable {
+        ethToToken(_minTokens, msg.sender);
+    }
+
+    function ethToTokenTransfer(uint256 _minTokens, address _recipient) public payable {
+        ethToToken(_minTokens, _recipient);
+    }
+
+    // Swap token to token!
+    function tokenToTokenSwap(
+        uint256 _tokensSold, 
+        uint256 _minTokensBought, 
+        address _tokenAddress
+        ) public {
+        /*
+            This is the SKM exchange
+            I want to buy DAI paying SKM
+            Which is the DAI exchange?
+        */
+        address exchangeAddress = Registry(registryAddress).getExchange(_tokenAddress);
+        require(exchangeAddress != address(this), "Invalid Exchange Address");
+        require(exchangeAddress != address(0), "Invalid Exchange Address");
+        uint256 tokenReserve = getReserve();
+        uint256 ethBought = _getAmount(_tokensSold, tokenReserve, address(this).balance);
+        IERC20(tokenAddress).transferFrom(msg.sender, address(this), _tokensSold);
+        Exchange(exchangeAddress).ethToTokenTransfer{value: ethBought}(_minTokensBought, msg.sender);
+    }    
 
 }
